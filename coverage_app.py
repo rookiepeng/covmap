b03cfdeb53a97b8bb0205964e3edf21af2a25fc4
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2024-01-12 21:32:35 -0500

    Update coverage_app.py

--- coverage_app.py
+++ coverage_app.py
@@ -32,7 +32,7 @@ import base64
 
 app = dash.Dash(
     __name__,
-    meta_tags=[{"name": "viewport", "content": "width=device-width,initial-scale=1"}],
+    meta_tags=[{"name": "viewport", "content": "width=device-width,height=device-height,initial-scale=1"}],
 )
 
 app.scripts.config.serve_locally = True
